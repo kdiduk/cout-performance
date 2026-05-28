2baf377aa187be2229c5f8c8b7cbc0948d3a4495
Kirill Diduk <kirill.diduk@gmail.com>
2026-05-28 08:10:55 +0200

    Cosmetic: clean up the code

--- main.cxx
+++ main.cxx
@@ -5,25 +5,23 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 
 std::vector<std::string> load_sonnets(const std::string& filename) {
-    std::ifstream iff(filename);
-    if (!iff.is_open()) {
-        std::cerr << "Failed to open file with sonnets: " << filename << std::endl;
-        std::exit(EXIT_FAILURE);
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::runtime_error("Failed to open file with sonnets: " + filename);
     }
 
     std::vector<std::string> sonnets;
-    while (iff) {
-        std::string line;
-        std::getline(iff, line);
-        sonnets.emplace_back(std::move(line));
+    for (std::string line; std::getline(file, line);) {
+        sonnets.push_back(std::move(line));
     }
 
-    return std::move(sonnets);
+    return sonnets;
 }
 
 
@@ -33,16 +31,17 @@ int main(int argc, char** argv) {
         filename = argv[1];
     }
 
-    auto&& sonnets = load_sonnets(filename);
+    auto sonnets = load_sonnets(filename);
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     for (int i = 0; i < 1000; ++i) {
         for (const auto& line: sonnets) {
+            std::cout << line << std::endl;
             // std::cout << line << '\n';
-            puts(line.c_str());
+            // puts(line.c_str());
         }
     }
-    std::cout.flush();
+    // std::cout.flush();
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
     std::cerr << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
